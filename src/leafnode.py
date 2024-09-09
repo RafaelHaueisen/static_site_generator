d9b06b74e773d98511c494aa980a2b708ed20e14
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-09-09 11:09:43 -0300

    Corrected a ValueError condition check

--- src/leafnode.py
+++ src/leafnode.py
@@ -5,7 +5,7 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value == None:
             raise ValueError("Invalid HTML: no value")
         if not self.tag:
             return self.value
