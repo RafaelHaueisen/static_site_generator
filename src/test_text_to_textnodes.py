776780aa5b5eaf44b2b8fa876eb0c9e8e3e9b2e6
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-09-17 09:46:19 -0300

    Created a tests file to text_to_textnodes file

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,31 @@
+import unittest
+from textnode import TextNode
+from text_to_textnodes import text_to_textnodes
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+class TestSplitNodes(unittest.TestCase):
+    def test_complex_input(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        result = text_to_textnodes(text)
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
